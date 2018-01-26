e576dd1daf7534551ec4d68335d11084ec91b280
krisven <867059031@qq.com>
2018-01-26 14:41:52 +0800

    get方式、load方式获取对象

--- src/com/test/Query.java
+++ src/com/test/Query.java
@@ -9,6 +9,8 @@ public class Query {
         Configuration conf = new Configuration().configure();
         SessionFactory sf = conf.buildSessionFactory();
         Session session = sf.openSession();
+
+        //get方式是非延迟加载，无论后面的代码是否会访问到属性，马上执行sql语句
         UserEntity userEntity = session.get(UserEntity.class, 2);
         int userId = userEntity.getId();
         String userUser = userEntity.getUsername();
@@ -16,6 +18,17 @@ public class Query {
         System.out.println("ID:" + userId);
         System.out.println("Username:" + userUser);
         System.out.println("Password:" + userPassword);
+
+        //load方式是延迟加载，只有属性被访问的时候才会调用sql语句
+        UserEntity userEntity1 = session.load(UserEntity.class, 1);
+        int userId1 = userEntity1.getId();
+        String userUser1 = userEntity1.getUsername();
+        String userPassword1 = userEntity1.getPassword();
+        System.out.println("ID:" + userId1);
+        System.out.println("Username:" + userUser1);
+        System.out.println("Password:" + userPassword1);
+
+
         session.close();
     }
 }
